2056a0076a257785de69c8e953acd89f111eaaad
PrathmeshBavge <prathmeshbavge8@gmail.com>
2022-10-19 08:30:21 -0700

    mera yarr prathmesh by hars

---
+++ index.js
@@ -0,0 +1,59 @@
+// prompt("enter") //this takes input from user.
+
+var village = 'latur';
+// console.log(village);
+
+let city = 'latur'
+{
+    let city = 'ausa'
+    // var village = 'almala'
+    // console.log(village)
+    console.log(city)
+}
+console.log(city)
+
+const fruit = 'mango'
+// fruit = 'orange'
+// const name;
+console.log(fruit)
+
+const arr = [23,4,4,5,5]
+// arr = [33,4,4]
+// arr.push(20)
+console.log(arr)
+
+// 1. not start with number 
+// 2. can start with both $ or _ 
+// 3. case sensitive //prathmesh Prathmesh prathMesh 
+
+// cases: 
+// snake_case //raduis_circle
+// kebab-case //hello-world
+// camelCase prathMesh
+// PascalCase PrathMesh
+
+// Datatype 
+let character = 'p';
+let ownerName = "Harsh n Prathmesh";
+let myNum = 93949;
+let now = new Date()
+let myObj = {
+    harsh: 100,
+    prathmesh: 88,
+    om: 39.0,
+    gutte: 00
+
+}
+let isProgammer = true
+console.log(ownerName, (typeof ownerName))
+console.log(character, (typeof character))
+console.log(myNum, (typeof myNum))
+console.log(isProgammer, (typeof isProgammer))
+console.log(now, (typeof now))
+console.log(myObj, (typeof myObj))
+console.table(myObj)
+
+
+// let name='BP';
+// console.log(name1);
+
---
+++ program.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout<<"Hello World!\n";
+    cout<<"Prathmesh and harsh are friends"<<endl;
+    return 0;
+}
+
